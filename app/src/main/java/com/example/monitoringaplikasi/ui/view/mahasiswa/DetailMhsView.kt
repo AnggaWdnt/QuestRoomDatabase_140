2724b49deae91d367324236017410759a2795f2c
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2024-12-16 00:09:48 +0700

    penambahan code function DetailMhsView.kt

--- app/src/main/java/com/example/monitoringaplikasi/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/monitoringaplikasi/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,2 +1,62 @@
 package com.example.monitoringaplikasi.ui.view.mahasiswa
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.monitoringaplikasi.ui.customwidget.TopAppBar
+import com.example.monitoringaplikasi.ui.viewmodel.DetailMhsViewModel
+import com.example.monitoringaplikasi.ui.viewmodel.PenyediaViewModel
+import java.lang.reflect.Modifier
+
+@Composable
+fun DetailMhsView(
+    modifier: Modifier = Modifier,
+    viewModel: DetailMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onBack: () -> Unit = { },
+    onEditClick: (String) -> Unit = { },
+    onDeleteClick: () -> Unit = { }
+){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                judul = "Detail Mahasiswa",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onEditClick(viewModel.detailUiState.value.detailUiEvent.nim) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa",
+                )
+            }
+        }
+    ) { innerPadding ->
+        val detailUiState by viewModel.detailUiState.collectAsState()
+
+        BodyDetailMhs(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+            onDeleteClick = {
+                viewModel.deleteMhs()
+                onDeleteClick()
+            }
+        )
+
+    }
+}
+
