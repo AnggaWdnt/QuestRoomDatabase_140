b39bb2ac6a984719b1c2596bb0c622d5f7aab30c
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2024-12-15 23:44:52 +0700

    penambahan code class UpdateMhsViewModel.kt

--- app/src/main/java/com/example/monitoringaplikasi/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/monitoringaplikasi/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,2 +1,31 @@
 package com.example.monitoringaplikasi.ui.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.monitoringaplikasi.repository.RepositoryMhs
+import com.example.monitoringaplikasi.ui.navigation.AlamatNavigasi
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class UpdateMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+    var updateUIState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[AlamatNavigasi.DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUIState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
+}
